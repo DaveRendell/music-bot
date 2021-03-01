44d5f327119199650f4e325158c590ade80b63c9
David Rendell <David.Rendell@Softwire.com>
2021-03-01 20:14:49 +0000

    Add express endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+import * as express from "express"
+
+const api = express()
+const port = 3000
+
+api.get("/", (req, res) => {
+  res.send("Hello, World!")
+})
+
+api.listen(port, () => {
+  console.log(`API running at http://localhost:${port}`)
+})
