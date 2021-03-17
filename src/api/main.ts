78338f89c7ae9cbcb88d99d1d9ccd2bdc0d71259
Sasha Burgoyne <sasha.burgoyne@softwire.com>
2021-03-17 19:37:09 +0000

    Adds song plays on voice channel

--- src/api/main.ts
+++ src/api/main.ts
@@ -4,10 +4,11 @@ import * as cors from "cors"
 import * as path from "path"
 import { RegisterRoutes } from "./build/routes"
 import { ValidateError } from "tsoa"
+import * as ytdl from "ytdl-core-discord"
+import * as Discord from "discord.js"
 
 const api = express()
 const port = 3000
-const Discord = require('discord.js')
 const client = new Discord.Client()
 const { token } = require('../../token.json')
 
@@ -16,8 +17,8 @@ api.use(bodyParser.json())
 api.use(cors())
 
 api.get("/sendTestMessage", (_req: express.Request, res: express.Response) => {
-  const channel = client.channels.cache.get('676831672230084608')
-  channel.send("Test message pls ignore")
+  const channel = client.channels.cache.get('676831672230084608') as Discord.TextChannel
+  channel?.send("Test message pls ignore")
   console.log("Button pressed")
   
   // Return empty response
@@ -61,5 +62,16 @@ client.once('ready', () => {
     console.log(`API & Discord running on port ${port}`)
   })
 })
+
+client.on('message', async (message: Discord.Message) => {
+	if (client.user && message.mentions.has(client.user)) {
+    if (message.member?.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      connection.play(await ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), { type: 'opus' });
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
 
 client.login(token)
